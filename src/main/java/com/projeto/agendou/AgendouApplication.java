5f9d3a42c6350267c571902029648c60e1a3cc99
galvessantos <gabrielww1@hotmail.com>
2025-12-02 20:38:11 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/projeto/agendou/AgendouApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agendou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendouApplication.class, args);
+	}
+
+}
